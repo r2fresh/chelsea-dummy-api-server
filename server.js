aabce0313cfdf1e5176b5dc162fcab31a645ffd4
r2fresh <r2fresh78@gmail.com>
2020-05-19 21:08:21 +0900

    port change

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(80, () => {
-  console.log("JSON Server is running");
+server.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
