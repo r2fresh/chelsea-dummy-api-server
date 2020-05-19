d1e8eb108b24cd8780755f0dcd893913601bf023
r2fresh <r2fresh78@gmail.com>
2020-05-19 21:16:32 +0900

    console.log error modify

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ server.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
-    app.settings.env
+    server.settings.env
   );
 });
