6511b1ce95b8efa372f9865aaf373668275fc5da
r2fresh <r2fresh78@gmail.com>
2020-05-19 20:51:02 +0900

    npm start add, server.js modify

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+server.listen(80, () => {
+  console.log("JSON Server is running");
+});
